10cc22358303e0c60d1f2ae9b3d241d21283dbbd
SR-Shahen <s.r.shahenda@gmail.com>
2022-04-08 23:52:59 +0600

    validation email and password

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import app from './firebase.init';
-import { createUserWithEmailAndPassword, getAuth } from 'firebase/auth'
+import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword } from 'firebase/auth'
 import { useState } from 'react';
 import { Form } from 'react-bootstrap';
 import { Button } from 'react-bootstrap';
@@ -11,8 +11,12 @@ const auth = getAuth(app);
 
 
 function App() {
+  const [validated, setValidated] = useState(false);
+  const [registird, setRegisterd] = useState(false);
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
 
   const handelEmailAdd = event => {
     setEmail(event.target.value);
@@ -22,16 +26,51 @@ function App() {
     setPassword(event.target.value);
 
   }
+  const handelChangeRegisterd = event => {
+    setRegisterd(event.target.checked);
+  }
 
   const handelSubmitFrom = (event) => {
-    createUserWithEmailAndPassword(auth, email, password)
-      .then(result => {
-        const user = result.user;
-        console.log(user);
-      })
-      .catch(error => {
-        console.log(error)
-      })
+
+    const form = event.currentTarget;
+    event.preventDefault();
+    if (form.checkValidity() === false) {
+      event.stopPropagation();
+      return;
+    }
+    if (!/(?=.*?[#?!@$%^&*-])/.test(password)) {
+      setError('Please should contain at least one specialist character ')
+      return;
+    }
+
+    setValidated(true);
+    setError('');
+    if (registird) {
+      signInWithEmailAndPassword(auth, email, password)
+        .then(result => {
+          const user = result.user;
+          console.log(user);
+        })
+        .catch(error => {
+
+          setError(error.message)
+        })
+
+    }
+    else {
+      createUserWithEmailAndPassword(auth, email, password)
+        .then(result => {
+          const user = result.user;
+          console.log(user);
+          setEmail('');
+          setPassword('');
+        })
+        .catch(error => {
+          console.log(error)
+          setError(error.message)
+        })
+    }
+
 
     event.preventDefault();
   }
@@ -39,23 +78,32 @@ function App() {
 
 
     <div className="registration w-50 mx-auto mt-4">
-      <h1 className='text-primary '>Please Register!!</h1>
-      <Form onSubmit={handelSubmitFrom}>
+      <h1 className='text-primary '>Please {registird ? "Log In" : "Register"}!!</h1>
+      <Form noValidate validated={validated} onSubmit={handelSubmitFrom}>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
-          <Form.Control onBlur={handelEmailAdd} type="email" placeholder="Enter email" />
+          <Form.Control onBlur={handelEmailAdd} type="email" placeholder="Enter email" required />
           <Form.Text className="text-muted">
             We'll never share your email with anyone else.
           </Form.Text>
+          <Form.Control.Feedback type="invalid">
+            Please provide a valid Email.
+          </Form.Control.Feedback>
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control onBlur={handelPasswordAdd} type="password" placeholder="Password" />
+          <Form.Control onBlur={handelPasswordAdd} type="password" placeholder="Password" required />
+          <Form.Control.Feedback type="invalid">
+            Please provide a valid password.
+          </Form.Control.Feedback>
         </Form.Group>
-
+        <Form.Group onChange={handelChangeRegisterd} className="mb-3" controlId="formBasicCheckbox">
+          <Form.Check type="checkbox" label="Already have an account" />
+        </Form.Group>
+        <h6 className='text-danger'>{error}</h6>
         <Button variant="primary" type="submit">
-          Submit
+          {registird ? "Log In" : "Register"}
         </Button>
       </Form>
 
